4a8cfe7cfefb0bd2ea20697e96fe24e1c285a27e
yuliduquedealba <yduquedealba@gmail.com>
2022-01-28 17:13:37 -0500

    First commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,42 @@
 const cipher = {
-  // ...
+    encode,
+    decode,
 };
+
+function encode(textOriginal, rango) {
+    let resultado = "";
+    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    rango = ((rango % 26) + 26) % 26;
+
+    if (textOriginal) {
+        for (let i = 0; i < textOriginal.length; i++) {
+            if (letras.indexOf(textOriginal[i]) != -1) {
+                let position = (letras.indexOf(textOriginal[i]) + rango) % 26;
+                resultado += letras[position];
+            } else {
+                resultado += textOriginal[i];
+            }
+        }
+        return resultado;
+    }
+}
+
+function decode(textOriginal, rango) {
+    let resultado = "";
+    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    rango = ((rango % 26) - 26) % 26;
+
+    if (textOriginal) {
+        for (let i = 0; i < textOriginal.length; i++) {
+            if (letras.indexOf(textOriginal[i]) != -1) {
+                let position = (letras.indexOf(textOriginal[i]) - rango) % 26;
+                resultado += letras[position];
+            } else {
+                resultado += textOriginal[i];
+            }
+        }
+        return resultado;
+    }
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import cipher from './cipher.js';
 
+function mayus() {
+    document.getElementById("text-original").addEventListener("keyup", function() {
+        this.value = this.value.toUpperCase();
+    });
+}
+mayus()
+
+document.getElementById("cifrar").addEventListener("click", function() {
+        let texto = document.getElementById("text-original").value;
+        let rango = document.getElementById("rango").value;
+
+        document.getElementById("text-result").value = cipher.encode(texto, rango);
+    },
+    true
+);
+
+document.getElementById("descifrar").addEventListener("click", function() {
+        let texto = document.getElementById("text-original").value;
+        let rango = document.getElementById("rango").value;
+
+        document.getElementById("text-result").value = cipher.decode(texto, rango);
+    },
+    true
+);
+
 console.log(cipher);
+
+
+
+/*
+<div id="limpiar">
+    <button id="reset" class="button">Limpiar</button>
+</div>
+
+document.getElementById("reset").addEventListener("click", function() {
+    let clean = document.getElementById("text-original, text-result") = "";
+    document.querySelector(clean);
+});
+*/
