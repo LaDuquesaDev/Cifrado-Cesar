d57e17cbcd6a10ad2d4a3987b7144e44e807dce0
yuliduquedealba <yduquedealba@gmail.com>
2022-01-31 14:06:32 -0500

    second commit

--- src/cipher.js
+++ src/cipher.js
@@ -3,40 +3,42 @@ const cipher = {
     decode,
 };
 
-function encode(textOriginal, rango) {
-    let resultado = "";
-    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    rango = ((rango % 26) + 26) % 26;
+//Funciones ENCODE (cifrar)
+function encode(rango, textOriginal) { //recibe los valores de los input
+    let resultado = ""; //Aqui se almacena el cifrado
+    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //Esta variable almacena el alfabeto con el que vamos a trabajar
+    rango = ((rango % 26) + 26) % 26; //El restante del rango en caso de ser negativo
 
-    if (textOriginal) {
-        for (let i = 0; i < textOriginal.length; i++) {
-            if (letras.indexOf(textOriginal[i]) != -1) {
-                let position = (letras.indexOf(textOriginal[i]) + rango) % 26;
-                resultado += letras[position];
-            } else {
-                resultado += textOriginal[i];
-            }
+    // if (textOriginal) {
+    for (let i = 0; i < textOriginal.length; i++) { //ciclo for | variable para determinar la longitud del texto | operador incremento
+        if (letras.indexOf(textOriginal[i]) != -1) { //condicional | indexOf devuelve el valor de la posición de la letra
+            let position = (letras.indexOf(textOriginal[i]) + rango) % 26; //Le suma la letra encontrada al rango para luego sumarle el restante
+            resultado += letras[position]; //Concatena el resultado (letras + posición)
+        } else {
+            resultado += textOriginal[i];
         }
-        return resultado;
     }
+    return resultado;
+    //}
 }
 
-function decode(textOriginal, rango) {
+//Funciones DECODE (descifrar)
+function decode(rango, textOriginal) {
     let resultado = "";
     let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     rango = ((rango % 26) - 26) % 26;
 
-    if (textOriginal) {
-        for (let i = 0; i < textOriginal.length; i++) {
-            if (letras.indexOf(textOriginal[i]) != -1) {
-                let position = (letras.indexOf(textOriginal[i]) - rango) % 26;
-                resultado += letras[position];
-            } else {
-                resultado += textOriginal[i];
-            }
+    //if (textOriginal) {
+    for (let i = 0; i < textOriginal.length; i++) {
+        if (letras.indexOf(textOriginal[i]) != -1) {
+            let position = (letras.indexOf(textOriginal[i]) - rango) % 26;
+            resultado += letras[position];
+        } else {
+            resultado += textOriginal[i];
         }
-        return resultado;
     }
+    return resultado;
+    //}
 }
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import cipher from './cipher.js';
 
+//Función que permite al usuario cambiar el texto a mayúsculas
 function mayus() {
     document.getElementById("text-original").addEventListener("keyup", function() {
         this.value = this.value.toUpperCase();
@@ -7,20 +8,22 @@ function mayus() {
 }
 mayus()
 
+//Botón cifrar
 document.getElementById("cifrar").addEventListener("click", function() {
         let texto = document.getElementById("text-original").value;
         let rango = document.getElementById("rango").value;
 
-        document.getElementById("text-result").value = cipher.encode(texto, rango);
+        document.getElementById("text-result").value = cipher.encode(rango, texto);
     },
     true
 );
 
+//Botón descifrar
 document.getElementById("descifrar").addEventListener("click", function() {
         let texto = document.getElementById("text-original").value;
         let rango = document.getElementById("rango").value;
 
-        document.getElementById("text-result").value = cipher.decode(texto, rango);
+        document.getElementById("text-result").value = cipher.decode(rango, texto);
     },
     true
 );
